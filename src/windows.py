c2570c049d30ff3dbd49860b389bd4a08040a47d
Dmitry Vorobjev <Dmitry.Vorobjev@systeme.ru>
2023-08-16 18:18:34 +0300

    added template for settings window

--- src/windows.py
+++ src/windows.py
@@ -5,7 +5,7 @@ import time
 
 import customtkinter
 import pystray
-from PIL import Image
+from PIL import Image, ImageTk
 
 from settings import Settings
 
@@ -181,7 +181,7 @@ class SettingsWnd(customtkinter.CTkToplevel):
         wnd_width = 500
         wnd_height = 400
         border_x = 50
-        border_y = 50
+        border_y = 150
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
         self.geometry(
@@ -192,8 +192,101 @@ class SettingsWnd(customtkinter.CTkToplevel):
         self.attributes("-topmost", True)
         self.resizable(False, False)
 
+        self.wnd_icon = ImageTk.PhotoImage(file="res/img/eyes_with_protection.png")
+        self.after(250, lambda: self.iconphoto(False, self.wnd_icon))
+
+        self.configure(fg_color="LightSteelBlue")
+
         self.protocol("WM_DELETE_WINDOW", self.hide)
         self.withdraw()
+
+        # window elements
+        # images
+        self.img_eyes_with_protection = customtkinter.CTkImage(
+            Image.open("res/img/eyes_with_protection.png"), size=(50, 50)
+        )
+
+        # set grid layout 1x2
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        # create navigation frame
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="SteelBlue")
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
+
+        self.navigation_frame_label = customtkinter.CTkLabel(
+            self.navigation_frame,
+            text=" Image Example",
+            # image=self.image,
+            compound="left",
+            font=customtkinter.CTkFont(size=15, weight="bold"),
+        )
+        self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+        self.navigation_frame_label = customtkinter.CTkLabel(
+            self.navigation_frame,
+            text="   EyesGuard",
+            image=self.img_eyes_with_protection,
+            compound="left",
+            font=customtkinter.CTkFont(size=15, weight="bold"),
+            # text_color="GreenYellow",
+        )
+        self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+        self.home_button = customtkinter.CTkButton(
+            self.navigation_frame,
+            corner_radius=0,
+            height=40,
+            border_spacing=10,
+            border_width=1,
+            text="Home",
+            fg_color="LightSteelBlue",
+            text_color=("gray10", "gray90"),
+            hover_color=("LightSkyBlue", "gray30"),
+            border_color="Gray",
+            # mage=self.image,
+            anchor="w",
+            # command=self.home_button_event,
+        )
+        self.home_button.grid(row=1, column=0, sticky="ew")
+
+        self.frame_2_button = customtkinter.CTkButton(
+            self.navigation_frame,
+            corner_radius=0,
+            height=40,
+            border_spacing=10,
+            border_width=1,
+            text="Frame 2",
+            fg_color="transparent",
+            text_color=("gray10", "gray90"),
+            hover_color=("LightSkyBlue", "gray30"),
+            border_color="Gray",
+            # image=self.image,
+            anchor="w",
+            # command=self.frame_2_button_event,
+        )
+        self.frame_2_button.grid(row=2, column=0, sticky="ew")
+
+        # create home frame
+        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.home_frame.grid_columnconfigure(1, weight=1)
+
+        self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="1111")
+        self.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
+
+        self.home_frame_button_1 = customtkinter.CTkButton(self.home_frame, text="btn1")
+        self.home_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
+        self.home_frame_button_2 = customtkinter.CTkButton(
+            self.home_frame, text="CTkButton", compound="right"
+        )
+        self.home_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
+
+        # create second frame
+        self.second_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+
+        # self.home_button.configure(fg_color=("gray75", "gray25"))
+        self.home_frame.grid(row=0, column=1, sticky="nsew")
 
     def on_focus_out(self, event):
         """Action in calse of loosing focus"""
