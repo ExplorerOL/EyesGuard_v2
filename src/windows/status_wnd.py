c73c2c6d73c4f998a90ac9e5e71d888aa57b0d6b
Dmitry Vorobjev <Dmitry.Vorobjev@systeme.ru>
2023-08-20 18:14:49 +0300

    interface elements were made smaller

--- src/windows/status_wnd.py
+++ src/windows/status_wnd.py
@@ -14,8 +14,8 @@ class StatusWnd(customtkinter.CTkToplevel):
     def __init__(self, settings: Settings, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.settings = settings
-        wnd_width = 280
-        wnd_height = 250
+        wnd_width = 250
+        wnd_height = 210
         border_x = 50
         border_y = 50
         self.attributes("-alpha", 0)
@@ -39,29 +39,15 @@ class StatusWnd(customtkinter.CTkToplevel):
 
         # label time until break
         self.lbl_time_until_break = customtkinter.CTkLabel(
-            self, text="Time until break:  - - -", font=("", 14, "bold")
+            self, text="Time until break:  - - -", font=("", 13, "bold")
         )
-        self.lbl_time_until_break.grid(row=0, column=0, padx=20, pady=10, sticky="ew")
+        self.lbl_time_until_break.grid(row=0, column=0, padx=20, pady=(20, 0), sticky="ew")
 
         # progress bar time until break
         self.pbar_time_until_break = customtkinter.CTkProgressBar(
             self, orientation="horizontal", height=20, fg_color="GreenYellow"
         )
-        self.pbar_time_until_break.grid(row=1, column=0, padx=20, pady=10, sticky="ew")
-
-        # button take a break
-        self.btn_take_break = customtkinter.CTkButton(
-            self,
-            text="Take a break now",
-            text_color="GreenYellow",
-            height=40,
-            width=150,
-            corner_radius=50,
-            image=customtkinter.CTkImage(light_image=self.img_check_mark, size=(40, 40)),
-            border_spacing=0,
-            font=("", 14, "bold"),
-        )
-        self.btn_take_break.grid(row=11, column=0, padx=20, pady=10, sticky="ew")
+        self.pbar_time_until_break.grid(row=1, column=0, padx=20, pady=(0, 10), sticky="ew")
 
         # button change protection state
         self.btn_change_protection_state = customtkinter.CTkButton(
@@ -69,14 +55,28 @@ class StatusWnd(customtkinter.CTkToplevel):
             text="Protection active",
             text_color="GreenYellow",
             command=self.btn_change_protection_state_action,
-            height=40,
+            height=30,
+            width=120,
+            corner_radius=50,
+            image=customtkinter.CTkImage(light_image=self.img_eyes_with_protection, size=(25, 25)),
+            border_spacing=0,
+            font=("", 13, "bold"),
+        )
+        self.btn_change_protection_state.grid(row=2, column=0, padx=20, pady=10, sticky="ew")
+
+        # button take a break
+        self.btn_take_break = customtkinter.CTkButton(
+            self,
+            text="Take a break now",
+            text_color="GreenYellow",
+            height=30,
             width=150,
             corner_radius=50,
-            image=customtkinter.CTkImage(light_image=self.img_eyes_with_protection, size=(40, 40)),
+            image=customtkinter.CTkImage(light_image=self.img_check_mark, size=(30, 30)),
             border_spacing=0,
-            font=("", 14, "bold"),
+            font=("", 13, "bold"),
         )
-        self.btn_change_protection_state.grid(row=10, column=0, padx=20, pady=10, sticky="ew")
+        self.btn_take_break.grid(row=3, column=0, padx=20, pady=10, sticky="ew")
 
         self.bind("<FocusOut>", self.on_focus_out)
         self.protocol("WM_DELETE_WINDOW", self.hide)
@@ -115,7 +115,7 @@ class StatusWnd(customtkinter.CTkToplevel):
             self.btn_change_protection_state.configure(
                 text="Protection active",
                 text_color="GreenYellow",
-                image=customtkinter.CTkImage(light_image=self.img_eyes_with_protection, size=(40, 40)),
+                image=customtkinter.CTkImage(light_image=self.img_eyes_with_protection, size=(30, 30)),
                 require_redraw=True,
             )
         else:
@@ -123,6 +123,6 @@ class StatusWnd(customtkinter.CTkToplevel):
                 text="Protection suspended",
                 text_color="Tomato",
                 # hover_color="yellow",
-                image=customtkinter.CTkImage(light_image=self.img_eyes_without_protection, size=(40, 40)),
+                image=customtkinter.CTkImage(light_image=self.img_eyes_without_protection, size=(30, 30)),
                 require_redraw=True,
             )
