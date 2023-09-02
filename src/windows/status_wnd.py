5d19f7efc19c547e23d8468fa1be99452de8c1ed
Dmitry Vorobjev <Dmitry.Vorobjev@systeme.ru>
2023-09-02 15:42:11 +0300

    settings window was added

--- src/windows/status_wnd.py
+++ src/windows/status_wnd.py
@@ -1,54 +1,11 @@
-"""Module with windows of application"""
+"""Module with status window of application"""
 
-import os
 import time
 
 import customtkinter
-import pystray
 from PIL import Image
 
 from settings import Settings
-
-
-class App(customtkinter.CTk):
-    """Main window of application"""
-
-    def __init__(self, settings: Settings):
-        super().__init__()
-        self.settings = settings
-        self.title("EyesGuard v2")
-        self.status_wnd = StatusWnd(settings)
-
-        # tray icon
-        self.image = Image.open("res/img/eyes_with_protection.png")
-        menu = (
-            pystray.MenuItem("Exit", self.exit_app),
-            pystray.MenuItem("Settings", self.show_settings_wnd),
-            pystray.MenuItem("Status", self.show_status_wnd, default=True),
-        )
-        self.icon = pystray.Icon("name", self.image, "My System Tray Icon1", menu)
-        self.icon.run_detached()
-
-        # hide main app wnd
-        self.withdraw()
-        self.toplevel_window = None
-        self.break_wnd = None
-
-    def exit_app(self):
-        """Exit from app"""
-        self.icon.visible = False
-        self.icon.stop()
-        self.quit()
-        os._exit(0)
-
-    def show_settings_wnd(self):
-        """Open settings wnd"""
-        print("Show settings wnd")
-
-    def show_status_wnd(self):
-        """Open status wnd"""
-        print("Show status wnd")
-        self.status_wnd.show()
 
 
 class StatusWnd(customtkinter.CTkToplevel):
@@ -57,17 +14,18 @@ class StatusWnd(customtkinter.CTkToplevel):
     def __init__(self, settings: Settings, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.settings = settings
-        wnd_width = 280
-        wnd_height = 250
+        wnd_width = 250
+        wnd_height = 210
         border_x = 50
         border_y = 50
+        self.attributes("-alpha", 0)
         screen_width = self.winfo_screenwidth()  # width of the screen
         screen_height = self.winfo_screenheight()  # height of the screen
         self.geometry(
             f"{wnd_width}x{wnd_height}+"
             f"{screen_width - wnd_width - border_x}+{screen_height - wnd_height - border_y}"
         )
-        self.title("EyesGuard v2 - Settings")
+        self.title("EyesGuard v2 - Status")
         self.attributes("-topmost", True)
         self.resizable(False, False)
         self.attributes("-toolwindow", True)
@@ -81,29 +39,15 @@ class StatusWnd(customtkinter.CTkToplevel):
 
         # label time until break
         self.lbl_time_until_break = customtkinter.CTkLabel(
-            self, text="Time until break:  - - -", font=("", 14, "bold")
+            self, text="Time until break:  - - -", font=("", 13, "bold")
         )
-        self.lbl_time_until_break.grid(row=0, column=0, padx=20, pady=10, sticky="ew")
+        self.lbl_time_until_break.grid(row=0, column=0, padx=20, pady=(20, 0), sticky="ew")
 
         # progress bar time until break
         self.pbar_time_until_break = customtkinter.CTkProgressBar(
             self, orientation="horizontal", height=20, fg_color="GreenYellow"
         )
-        self.pbar_time_until_break.grid(row=1, column=0, padx=20, pady=10, sticky="ew")
-
-        # button take a break
-        self.btn_take_break = customtkinter.CTkButton(
-            self,
-            text="Take a break now",
-            text_color="GreenYellow",
-            height=40,
-            width=150,
-            corner_radius=50,
-            image=customtkinter.CTkImage(light_image=self.img_check_mark, size=(40, 40)),
-            border_spacing=0,
-            font=("", 14, "bold"),
-        )
-        self.btn_take_break.grid(row=11, column=0, padx=20, pady=10, sticky="ew")
+        self.pbar_time_until_break.grid(row=1, column=0, padx=20, pady=(0, 10), sticky="ew")
 
         # button change protection state
         self.btn_change_protection_state = customtkinter.CTkButton(
@@ -111,14 +55,28 @@ class StatusWnd(customtkinter.CTkToplevel):
             text="Protection active",
             text_color="GreenYellow",
             command=self.btn_change_protection_state_action,
-            height=40,
+            height=30,
+            width=120,
+            corner_radius=50,
+            image=customtkinter.CTkImage(light_image=self.img_eyes_with_protection, size=(25, 25)),
+            border_spacing=0,
+            font=("", 13, "bold"),
+        )
+        self.btn_change_protection_state.grid(row=2, column=0, padx=20, pady=10, sticky="ew")
+
+        # button take a break
+        self.btn_take_break = customtkinter.CTkButton(
+            self,
+            text="Take a break now",
+            text_color="GreenYellow",
+            height=30,
             width=150,
             corner_radius=50,
-            image=customtkinter.CTkImage(light_image=self.img_eyes_with_protection, size=(40, 40)),
+            image=customtkinter.CTkImage(light_image=self.img_check_mark, size=(30, 30)),
             border_spacing=0,
-            font=("", 14, "bold"),
+            font=("", 13, "bold"),
         )
-        self.btn_change_protection_state.grid(row=10, column=0, padx=20, pady=10, sticky="ew")
+        self.btn_take_break.grid(row=3, column=0, padx=20, pady=10, sticky="ew")
 
         self.bind("<FocusOut>", self.on_focus_out)
         self.protocol("WM_DELETE_WINDOW", self.hide)
@@ -130,16 +88,16 @@ class StatusWnd(customtkinter.CTkToplevel):
         self.hide()
 
     def hide(self):
-        """Hide status window"""
+        """Hide window"""
         for i in range(100):
             self.attributes("-alpha", 1 - i / 100)
             time.sleep(0.006)
         self.withdraw()
 
     def show(self):
-        """Show status window"""
-        print("showing top level wnd")
-
+        """Show window"""
+        print("Showing status wnd")
+        self.update_wnd()
         self.deiconify()
 
         for i in range(100):
@@ -157,7 +115,7 @@ class StatusWnd(customtkinter.CTkToplevel):
             self.btn_change_protection_state.configure(
                 text="Protection active",
                 text_color="GreenYellow",
-                image=customtkinter.CTkImage(light_image=self.img_eyes_with_protection, size=(40, 40)),
+                image=customtkinter.CTkImage(light_image=self.img_eyes_with_protection, size=(30, 30)),
                 require_redraw=True,
             )
         else:
@@ -165,14 +123,6 @@ class StatusWnd(customtkinter.CTkToplevel):
                 text="Protection suspended",
                 text_color="Tomato",
                 # hover_color="yellow",
-                image=customtkinter.CTkImage(light_image=self.img_eyes_without_protection, size=(40, 40)),
+                image=customtkinter.CTkImage(light_image=self.img_eyes_without_protection, size=(30, 30)),
                 require_redraw=True,
             )
-
-
-class SettingsWindow:
-    pass
-
-
-class SplashWindow:
-    pass
