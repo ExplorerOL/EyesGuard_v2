024891bf5811bd31f79b367d37c2fcf31330cd35
Dmitry Vorobjev <Dmitry.Vorobjev@systeme.ru>
2023-09-03 21:15:59 +0300

    steps list creation was improved

--- src/control_alg.py
+++ src/control_alg.py
@@ -36,7 +36,12 @@ class ControlAlg:
     def __init__(self, settings: Settings):
         step_notified_duration_s = 60
 
-        self.steps = [StepData() for item in StepType]
+        self.steps: list[StepData] = []
+
+        for step_type in StepType:
+            self.steps.insert(step_type, StepData())
+            self.steps[step_type].step_type = step_type
+
         print(self.steps)
         print(type(self.steps))
 
@@ -45,19 +50,15 @@ class ControlAlg:
         print(StepType.work_mode)
         print(StepType.work_notified)
 
-        self.steps[1].step_type = StepType.work_mode
-        self.steps[1].step_duration_s = user_settings.work_duration
-
-        # self.steps[StepType.work_notified].step_type = StepType.work_notified
-        # self.steps[StepType.work_notified].step_duration_s = step_notified_duration_s
-
-        # self.steps[StepType.break_mode].step_type = StepType.break_mode
-        # self.steps[StepType.break_mode].step_duration_s = user_settings.break_duration
-
-        # self.steps[StepType.break_notified].step_type = StepType.break_notified
-        # self.steps[StepType.break_notified].step_duration_s = step_notified_duration_s
+        self.steps[StepType.work_mode].step_duration_s = user_settings.work_duration * 60
+        self.steps[StepType.work_notified].step_duration_s = step_notified_duration_s
+        self.steps[StepType.break_mode].step_duration_s = user_settings.break_duration * 60
+        self.steps[StepType.break_notified].step_duration_s = step_notified_duration_s
 
         print(self.steps)
+        for step in self.steps:
+            print(step.step_type)
+            print(step.step_duration_s)
 
         self.current_step = CurrentStep()
         if user_settings.protection_status == "off":
