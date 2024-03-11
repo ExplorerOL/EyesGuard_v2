241bee5954ab576e77f393d458c241cb957a1eb4
Dmitry Vorobjev <Dmitry.Vorobjev@systeme.ru>
2024-03-11 09:15:29 +0300

    обход циклических зависимостей

--- src/model.py
+++ src/model.py
@@ -1,3 +1,11 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from view import EGView
+
+# import view
 from settings import Settings
 from states import CurrentState
 
@@ -8,10 +16,10 @@ class EGModel:
         self.settings = Settings(settings_file)
         self.current_state = CurrentState(self.settings)
 
-    def set_view(self, view: object):
+    def set_view(self, view: EGView):
         """Assigning controller to view"""
 
         # import placed here for breaking circular import
-        from view import EGView
+        # from view import EGView
 
         self.view = view
